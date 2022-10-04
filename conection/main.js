fbbad8b65dd35b73726767a05e8be726efde56bf
Harvey Unsaac <193837@unsaac.edu.pe>
2022-10-04 18:51:53 +0200

    :sparkles: Add a reed html into js

--- conection/main.js
+++ conection/main.js
@@ -1,1 +1,16 @@
 console.log('Esto es desde main.js');
+
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const paragraph = document.querySelector('.paragraph');
+const pid = document.querySelector('#identifier');
+const input = document.querySelector('input');
+
+console.log(h1);
+console.log({
+    h1,
+    p,
+    paragraph,
+    pid,
+    input
+})
