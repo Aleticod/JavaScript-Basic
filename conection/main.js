89aa7d2c20cc84d5739cc21a5487103ddb330c99
Harvey Unsaac <193837@unsaac.edu.pe>
2022-10-05 04:31:59 +0200

    :sparkles: Add how to modify a DOM from js

--- conection/main.js
+++ conection/main.js
@@ -17,3 +17,19 @@ console.log({
 
 console.log(input.value);
 console.log(document.querySelector('input').value);
+
+h1.innerHTML = 'Este titulo se escribio desde js';
+
+h1.classList.add('title');
+input.value = 'Default value from js';
+
+// Create a new element
+
+const img = document.createElement('img');
+img.setAttribute('src', 'https://static.platzi.com/static/images/conf/logo_black@2x.png');
+
+console.log(img);
+
+// Insert the img object into the p with atribute
+pid.innerHTML = "";
+pid.replaceWith(img)
