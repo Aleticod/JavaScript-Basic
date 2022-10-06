5654d13293b02adbe32d8b7ca9a283d792ae67d9
Harvey Unsaac <ail>
2022-10-06 16:37:57 +0200

    :sparkles: Add the mobile menu

--- store-app/main.js
+++ store-app/main.js
@@ -5,9 +5,24 @@ const $ = (selector) => document.querySelector(selector);
 
 const menuEmail = $('.navbar-email');
 const desktopMenu = $('.desktop-menu');
+const menuHamIcon = $('.menu');
+const mobileMenu = $('.mobile-menu');
 
-menuEmail.addEventListener('click', toggleDesktopMenu);
 
-function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive');
+// menuEmail.addEventListener('click', toggleDesktopMenu);
+// menuHamIcon.addEventListener('click', toggleMobileMenu);
+
+// function toggleDesktopMenu() {
+//     desktopMenu.classList.toggle('inactive');
+// }
+
+// function toggleMobileMenu() {
+//     mobileMenu.classList.toggle('inactive');
+// }
+
+menuEmail.addEventListener('click', () => {toggleMenu(desktopMenu)});
+menuHamIcon.addEventListener('click', () => {toggleMenu(mobileMenu)});
+
+function toggleMenu(menu) {
+    menu.classList.toggle('inactive');
 }
