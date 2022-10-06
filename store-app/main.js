289a6f3d20f7126bb5a5edd1fb2a3885de3c0dbe
Harvey Unsaac <ail>
2022-10-06 18:20:33 +0200

    :sparkles: Add the product details menu

--- store-app/main.js
+++ store-app/main.js
@@ -3,10 +3,13 @@
 
 const $ = (selector) => document.querySelector(selector);
 
+//const mainContainer = $('body');
 const menuEmail = $('.navbar-email');
 const desktopMenu = $('.desktop-menu');
 const menuHamIcon = $('.menu');
 const mobileMenu = $('.mobile-menu');
+const menuCarIcon = $('.navbar-shopping-cart');
+const productDetail = $('.product-detail');
 
 
 // menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -20,9 +23,46 @@ const mobileMenu = $('.mobile-menu');
 //     mobileMenu.classList.toggle('inactive');
 // }
 
+//mainContainer.addEventListener('click', closeMenus);
 menuEmail.addEventListener('click', () => {toggleMenu(desktopMenu)});
-menuHamIcon.addEventListener('click', () => {toggleMenu(mobileMenu)});
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+//menuCarIcon.addEventListener('click', () => {toggleMenu(productDetail)});
+menuCarIcon.addEventListener('click', toggleCarAside);
+
+function closeMenus() {
+    if (!desktopMenu.classList.contains('inactive')) {
+        toggleMenu(desktopMenu);
+    }
+
+    if (!mobileMenu.classList.contains('inactive')) {
+        toggleMenu(mobileMenu);
+    }
+
+    if (!productDetail.classList.contains('inactive')) {
+        toggleMenu(productDetail);
+    }
+}
 
 function toggleMenu(menu) {
     menu.classList.toggle('inactive');
+}
+
+function toggleCarAside() {
+    const isMobileMenuClose = mobileMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClose) {
+        toggleMenu(mobileMenu);
+    }
+
+    toggleMenu(productDetail);
+}
+
+function toggleMobileMenu() {
+    const isProductDetailClose = productDetail.classList.contains('inactive');
+
+    if (!isProductDetailClose) {
+        toggleMenu(productDetail);
+    }
+
+    toggleMenu(mobileMenu);
 }
